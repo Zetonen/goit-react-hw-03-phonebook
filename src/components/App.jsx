42ec4f60a473dd58e8f1056784b6c9346bd7c25d
Seven <graf.lancelot7@gmail.com>
2023-08-22 22:20:06 +0300

    add localStorage api

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
 import { nanoid } from 'nanoid';
+import { getContacts, addContacts } from 'services/contact-api';
 import { Section, Title, SubTitle } from './App.styled';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = getContacts();
+    console.log(contacts);
+    if (contacts) {
+      this.setState({ contacts });
+    }
+  }
   addContact = contact => {
     const isInContacts = this.state.contacts.find(
       ({ name }) => name.toLowerCase() === contact.name.toLowerCase()
@@ -24,8 +27,10 @@ export class App extends Component {
       return;
     }
     this.setState(prevState => {
+      const contacts = [...prevState.contacts, { ...contact, id: nanoid() }];
+      addContacts(contacts);
       return {
-        contacts: [...prevState.contacts, { ...contact, id: nanoid() }],
+        contacts,
       };
     });
   };
@@ -45,8 +50,10 @@ export class App extends Component {
 
   onDeleteContact = contactId => {
     this.setState(prevState => {
+      const contacts = prevState.contacts.filter(({ id }) => id !== contactId);
+      addContacts(contacts);
       return {
-        contacts: prevState.contacts.filter(({ id }) => id !== contactId),
+        contacts,
       };
     });
   };
